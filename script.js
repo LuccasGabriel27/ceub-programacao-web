bbac812d288b7fdf8df3c3c34e380967d579209c
LuccasGabriel27 <luccasgabriel862@gmail.com>
2024-03-20 09:10:31 -0300

    javaScript

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("teste")
+
+let aceitouCookies = false;
+
+let teste = 1;
+let variavelString = "Ola, mundo";
+
+console.log(teste);
+console.log(variavelString)
+
+teste = "abc"
+
+console.log(typeof(aceitouCookies))
