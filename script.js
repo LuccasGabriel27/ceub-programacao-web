d03c179b62e003cd218c166b6c8020fe4d6aac50
LuccasGabriel27 <luccasgabriel862@gmail.com>
2024-03-20 10:14:09 -0300

    javaScriptAtt

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-// console.log("teste")
+// // console.log("teste")
 
-// let aceitouCookies = false;
+// // let aceitouCookies = false;
 
-// let teste = 1;
-// let variavelString = "Ola, mundo";
+// // let teste = 1;
+// // let variavelString = "Ola, mundo";
 
-// console.log(teste);
-// console.log(variavelString)
+// // console.log(teste);
+// // console.log(variavelString)
 
-// teste = "abc"
+// // teste = "abc"
 
-//  console.log(typeof(aceitouCookies))
+// //  console.log(typeof(aceitouCookies))
+
+// let idade = 17;
+// if (idade >= 18){
+//     console.log("usuario pode entrar no site");
+// } else{
+//     console.log("usuario pode entrar no site")
+// }
 
 let idade = 17;
-if (idade >= 18){
-    console.log("usuario pode entrar no site");
-} else{
-    console.log("usuario pode entrar no site")
+let nome = "joao";
+let altura = 1.80;
+
+if(idade >= 18 || nome != "pedro"){
+    console.log("ola!");
 }
+
+let meuArray = [1, 3, 4, 6, 0, -3];
+
+console.log(meuArray[0]);
+console.log(meuArray);
+
+console.log(meuArray.length)
