b7186a8dc182a78f359d4ce8aed2b1c54a2f9d61
LuccasGabriel27 <luccasgabriel862@gmail.com>
2024-03-20 09:35:57 -0300

    javaScript2

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-console.log("teste")
+// console.log("teste")
 
-let aceitouCookies = false;
+// let aceitouCookies = false;
 
-let teste = 1;
-let variavelString = "Ola, mundo";
+// let teste = 1;
+// let variavelString = "Ola, mundo";
 
-console.log(teste);
-console.log(variavelString)
+// console.log(teste);
+// console.log(variavelString)
 
-teste = "abc"
+// teste = "abc"
 
-console.log(typeof(aceitouCookies))
+//  console.log(typeof(aceitouCookies))
+
+let idade = 17;
+if (idade >= 18){
+    console.log("usuario pode entrar no site");
+} else{
+    console.log("usuario pode entrar no site")
+}
